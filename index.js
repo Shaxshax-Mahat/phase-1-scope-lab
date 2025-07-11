eb97e137476d0486d687741d3c348da0a3c49460
Shaxshax-Mahat <abdulrahmanmahat0@gmail.com>
2025-07-11 23:19:26 +0300

    Complete phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // no var/let/const => creates global variable (bad practice, but intentional here)
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Karen';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Not Karen'; // This will throw an error
+}
+
+
+
